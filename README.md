3b9065395d418c489e8f5c9b589cba8bff50eb7e
Hardikkr <hardik.kumar18feb@gmail.com>
2020-11-19 14:40:06 +0530

    readme and license added, removed unused buttons

---
+++ README.md
@@ -0,0 +1,11 @@
+![](https://static.vecteezy.com/system/resources/previews/001/233/723/non_2x/cartoon-positive-people-standing-in-queue-at-atm-vector.jpg)
+# Secure ATM
+This project is made to make **Automated teller machine** more secure. It's simple nowadays to get your pin using hidden cameras, false keyboards, etc. So, here we are with a different idea to let your pin secure.
+
+### Steps to Run
+1. Fork this Repo
+2. Clone that forked repo into your local system
+3. Run **code.py** file
+
+### Support
+If you like this project, don't forget to give it a ⭐
--- code.py
+++ code.py
@@ -2,77 +2,66 @@ from tkinter import *
 
 window = Tk()
 window.title("ATM")
-window.resizable(width="false", height="true")
+window.resizable(width="false", height="false")
 
 equation = StringVar()
-
 expression = ""
 
-def press(num):
+def pressButton(num):
     global expression
     expression = expression + str(num)
     equation.set(expression)
 
-
-def clear():
+def clearAll():
     global expression
     expression = ""
     equation.set("")
 
-def backspace():
+def clearOne():
     global expression
     expression = expression[:-1]
     equation.set(expression)
 
-
 input_field = Entry(window, textvariable = equation)
-input_field.grid(columnspan=5, ipadx=50 ,ipady=10, padx=5,pady=5)
+input_field.grid(columnspan=5, ipadx=50 , ipady=10, padx=5, pady=5)
 
-button1 = Button(window, text='1', fg='black', bg='red', command=lambda:press(1), height=2, width=5)
-button1.grid(row=1,column=0,padx=5, pady=5)
+button1 = Button(window, text='1', fg='black', bg='red', command=lambda:pressButton(1), height=2, width=5)
+button1.grid(row=1, column=0, padx=5, pady=5)
 
-button2 = Button(window, text='2', fg='black', bg='green', command=lambda:press(2), height=2, width=5)
-button2.grid(row=1,column=1,padx=5, pady=5)
+button2 = Button(window, text='2', fg='black', bg='green', command=lambda:pressButton(2), height=2, width=5)
+button2.grid(row=1, column=1, padx=5, pady=5)
 
-button3 = Button(window, text='3', fg='black', bg='blue', command=lambda:press(3), height=2, width=5)
-button3.grid(row=1,column=2,padx=5, pady=5)
+button3 = Button(window, text='3', fg='black', bg='blue', command=lambda:pressButton(3), height=2, width=5)
+button3.grid(row=1, column=2, padx=5, pady=5)
 
-button4 = Button(window, text='4', fg='black', bg='yellow', command=lambda:press(4), height=2, width=5)
-button4.grid(row=2,column=0,padx=5, pady=5)
+button4 = Button(window, text='4', fg='black', bg='yellow', command=lambda:pressButton(4), height=2, width=5)
+button4.grid(row=2, column=0, padx=5, pady=5)
 
-button5 = Button(window, text='5', fg='black', bg='pink', command=lambda:press(5), height=2, width=5)
-button5.grid(row=2,column=1,padx=5, pady=5)
+button5 = Button(window, text='5', fg='black', bg='pink', command=lambda:pressButton(5), height=2, width=5)
+button5.grid(row=2, column=1, padx=5, pady=5)
 
-button6 = Button(window, text='6', fg='black', bg='orange', command=lambda:press(6), height=2, width=5)
-button6.grid(row=2,column=2,padx=5, pady=5)
+button6 = Button(window, text='6', fg='black', bg='orange', command=lambda:pressButton(6), height=2, width=5)
+button6.grid(row=2, column=2, padx=5, pady=5)
 
-button7 = Button(window, text='7', fg='black', bg='purple', command=lambda:press(7), height=2, width=5)
-button7.grid(row=3,column=0,padx=5, pady=5)
+button7 = Button(window, text='7', fg='black', bg='purple', command=lambda:pressButton(7), height=2, width=5)
+button7.grid(row=3, column=0, padx=5, pady=5)
 
-button8 = Button(window, text='8', fg='black', bg='grey', command=lambda:press(8), height=2, width=5)
-button8.grid(row=3,column=1,padx=5, pady=5)
+button8 = Button(window, text='8', fg='black', bg='grey', command=lambda:pressButton(8), height=2, width=5)
+button8.grid(row=3, column=1, padx=5, pady=5)
 
-button9 = Button(window, text='9', fg='black', bg='magenta', command=lambda:press(9), height=2, width=5)
-button9.grid(row=3,column=2,padx=5, pady=5)
+button9 = Button(window, text='9', fg='black', bg='magenta', command=lambda:pressButton(9), height=2, width=5)
+button9.grid(row=3, column=2, padx=5, pady=5)
 
-button10 = Button(window, text='', fg='black', bg='white', height=2, width=5)
-button10.grid(row=4,column=0,padx=5, pady=5)
+button0 = Button(window, text='0', fg='black', bg='light green', command=lambda:pressButton(0), height=2, width=5)
+button0.grid(row=4, column=1, padx=5, pady=5)
 
-button11 = Button(window, text='0', fg='black', bg='light green', command=lambda:press(0), height=2, width=5)
-button11.grid(row=4,column=1,padx=5, pady=5)
+backspace = Button(window, text='BACKSPACE', fg='black', bg='light grey', command=clearOne, height=2, width=10)
+backspace.grid(row=1, column=4, padx=5, pady=5)
 
-button12 = Button(window, text='', fg='black', bg='white', height=2, width=5)
-button12.grid(row=4,column=2,padx=5, pady=5)
+clear = Button(window, text='CLEAR', fg='black', bg='light grey', command=clearAll, height=2, width=8)
+clear.grid(row=2, column=4, padx=5, pady=5)
 
-button13 = Button(window, text='CANCEL', fg='black', bg='white',command=clear, height=2, width=8)
-button13.grid(row=1,column=4,padx=5, pady=5)
+enter = Button(window, text='ENTER', fg='black', bg='light grey', height=2, width=8)
+enter.grid(row=3, column=4, padx=5, pady=5)
 
-button14 = Button(window, text='ENTER', fg='black', bg='white', height=2, width=8)
-button14.grid(row=2,column=4,padx=5, pady=5)
-
-button15 = Button(window, text='CUT', fg='black', bg='white',command=backspace, height=2, width=8)
-button15.grid(row=3,column=4,padx=5, pady=5)
-
-button16 = Button(window, text=' ', fg='black', bg='white', height=2, width=8)
-button16.grid(row=4,column=4,padx=5, pady=5)
 window.mainloop()
