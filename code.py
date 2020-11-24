d0eaf450b8586eb8623b15ea9b13bd1c598cc583
AnkitKumarvaid <61087993+AnkitKumarvaid@users.noreply.github.com>
2020-11-24 22:24:39 +0530

    retry function added

--- code.py
+++ code.py
@@ -20,27 +20,33 @@ correctPin = '6344'
 screenNumber = 0
 
 # Number list
-numlist = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-newlist = numlist
+normalList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+shuffledList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+random.shuffle(shuffledList)
+
+globalList = normalList
 
 # For creating number table
 class Table:
     def __init__(self, page):
+        global globalList
 
-        if screenNumber == 2:
-            random.shuffle(newlist)
+        if(screenNumber==1):
+            globalList = normalList
+        elif(screenNumber==2):
+            globalList = shuffledList
 
         # take the data
-        lst = [('Red', newlist[0]),
-               ('Dark Green', newlist[1]),
-               ('Blue', newlist[2]),
-               ('Yellow', newlist[3]),
-               ('Brown', newlist[4]),
-               ('Orange', newlist[5]),
-               ('Purple', newlist[6]),
-               ('Grey', newlist[7]),
-               ('Pink', newlist[8]),
-               ('Light Green', newlist[9])]
+        lst = [('Red', globalList[0]),
+               ('Dark Green', globalList[1]),
+               ('Blue', globalList[2]),
+               ('Yellow', globalList[3]),
+               ('Brown', globalList[4]),
+               ('Orange', globalList[5]),
+               ('Purple', globalList[6]),
+               ('Grey', globalList[7]),
+               ('Pink', globalList[8]),
+               ('Light Green', globalList[9])]
 
         newFrame = LabelFrame(page)
         newFrame.grid(columnspan=5)
@@ -53,10 +59,10 @@ class Table:
                     e = Text(newFrame, height=3, width=6, bg=bgColor)
                     e.grid(row=i, column=j)
                 else:
-                    e = Text(newFrame, height=2, width=5, fg='Black', font=('Arial', 10), padx=8, pady=4)
+                    e = Text(newFrame, height=2, width=5, fg='Black', font=('Ariel', 10), padx=8, pady=4)
                     e.grid(row=i, column=j)
                     e.insert(END, lst[j][i])
-                
+
 # For INTEGER buttons
 def pressButton(num):
     global atmIdText, pinText
@@ -141,10 +147,29 @@ def goToPage1():
                 finalText = 'Failed'
 
             page3 = LabelFrame(mainFrame)
-            page3.grid(columnspan=5, ipadx=140, ipady=90, padx=10, pady=10)
+            page3.grid(columnspan=5, ipadx=270, ipady=110, padx=10, pady=10)
             
-            finalText = Label(page3, text=finalText)
-            finalText.place(relx=0.5, rely=0.5, anchor="center")
+            finalText = Label(page3, text=finalText, font=('Courier', 20))
+            finalText.place(relx=0.5, rely=0.35, anchor="center")
+
+            def retry():
+                global atmIdText, pinText
+                page3.destroy()
+                atmIdText = ""
+                atmIdFieldKey.set(atmIdText)
+                pinText = ""
+                pinFieldKey.set(pinText)
+                goToPage1()
+
+            if ((atmIdText == correctAtmId) and (pinText == correctPin)):
+                exitButton = Button(page3, text='Exit', bg='White', height=2, width=8, command=window.destroy)
+                exitButton.place(relx=0.5, rely=0.65, anchor="center")
+            else:
+                retryButton = Button(page3, text='Retry', bg='white', height=2, width=8,command=lambda:retry())
+                retryButton.place(relx=0.4, rely=0.65, anchor="center")            
+
+                exitButton = Button(page3, text='Exit', bg='White', height=2, width=8, command=window.destroy)
+                exitButton.place(relx=0.6, rely=0.65, anchor="center")
 
 
 # Main frame
@@ -153,43 +178,43 @@ mainFrame.grid(columnspan=5)
 
 # Page 0
 page0 = LabelFrame(mainFrame)
-page0.grid(columnspan=5, ipadx=140, ipady=90, padx=10, pady=10)
+page0.grid(columnspan=5, ipadx=270, ipady=110, padx=10, pady=10)
 
-welcomeText = Label(page0, text='WELCOME TO XYZ BANK ATM')
-welcomeText.place(relx=0.5, rely=0.3, anchor="center")
+welcomeText = Label(page0, text='Welcome to INAXIA bank atm', font=('Courier', 20))
+welcomeText.place(relx=0.5, rely=0.35, anchor="center")
 
 nextbutton = Button(page0, text='Next', bg='White', height=2, width=10, command=lambda: goToPage1())
-nextbutton.place(relx=0.5, rely=0.7, anchor="center")
+nextbutton.place(relx=0.5, rely=0.65, anchor="center")
 
 # All buttons
-button1 = Button(window, fg='Black', bg='Red', command=lambda: pressButton(newlist[0]), height=2, width=5)
+button1 = Button(window, fg='Black', bg='Red', command=lambda: pressButton(globalList[0]), height=2, width=5)
 button1.grid(row=1, column=0, padx=5, pady=5)
 
-button2 = Button(window, fg='Black', bg='Dark Green', command=lambda: pressButton(newlist[1]), height=2, width=5)
+button2 = Button(window, fg='Black', bg='Dark Green', command=lambda: pressButton(globalList[1]), height=2, width=5)
 button2.grid(row=1, column=1, padx=5, pady=5)
 
-button3 = Button(window, fg='Black', bg='Blue', command=lambda: pressButton(newlist[2]), height=2, width=5)
+button3 = Button(window, fg='Black', bg='Blue', command=lambda: pressButton(globalList[2]), height=2, width=5)
 button3.grid(row=1, column=2, padx=5, pady=5)
 
-button4 = Button(window, fg='Black', bg='Yellow', command=lambda: pressButton(newlist[3]), height=2, width=5)
+button4 = Button(window, fg='Black', bg='Yellow', command=lambda: pressButton(globalList[3]), height=2, width=5)
 button4.grid(row=2, column=0, padx=5, pady=5)
 
-button5 = Button(window, fg='Black', bg='Brown', command=lambda: pressButton(newlist[4]), height=2, width=5)
+button5 = Button(window, fg='Black', bg='Brown', command=lambda: pressButton(globalList[4]), height=2, width=5)
 button5.grid(row=2, column=1, padx=5, pady=5)
 
-button6 = Button(window, fg='Black', bg='Orange', command=lambda: pressButton(newlist[5]), height=2, width=5)
+button6 = Button(window, fg='Black', bg='Orange', command=lambda: pressButton(globalList[5]), height=2, width=5)
 button6.grid(row=2, column=2, padx=5, pady=5)
 
-button7 = Button(window, fg='Black', bg='Purple', command=lambda: pressButton(newlist[6]), height=2, width=5)
+button7 = Button(window, fg='Black', bg='Purple', command=lambda: pressButton(globalList[6]), height=2, width=5)
 button7.grid(row=3, column=0, padx=5, pady=5)
 
-button8 = Button(window, fg='Black', bg='Grey', command=lambda: pressButton(newlist[7]), height=2, width=5)
+button8 = Button(window, fg='Black', bg='Grey', command=lambda: pressButton(globalList[7]), height=2, width=5)
 button8.grid(row=3, column=1, padx=5, pady=5)
 
-button9 = Button(window, fg='Black', bg='Pink', command=lambda: pressButton(newlist[8]), height=2, width=5)
+button9 = Button(window, fg='Black', bg='Pink', command=lambda: pressButton(globalList[8]), height=2, width=5)
 button9.grid(row=3, column=2, padx=5, pady=5)
 
-button0 = Button(window, fg='Black', bg='Light Green', command=lambda: pressButton(newlist[9]), height=2, width=5)
+button0 = Button(window, fg='Black', bg='Light Green', command=lambda: pressButton(globalList[9]), height=2, width=5)
 button0.grid(row=4, column=1, padx=5, pady=5)
 
 backspace = Button(window, text='BACKSPACE', fg='Black', bg='Light Grey', command=lambda: clearOne(), height=2, width=10)
